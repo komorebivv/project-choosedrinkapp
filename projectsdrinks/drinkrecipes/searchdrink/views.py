ea5a21b95c9c3229fc43aa65a1e37fc1089b26e5
komorebivv <patrycja.stanislawczyk@gmail.com>
2021-09-06 19:42:56 +0200

    changing name of variables to more readable

--- projectsdrinks/drinkrecipes/searchdrink/views.py
+++ projectsdrinks/drinkrecipes/searchdrink/views.py
@@ -7,17 +7,15 @@ from django.core import serializers
 import json
 
 
-
 def mainpage(request):
-    context={}
+    context = {}
     return render(request, 'searchdrink/mainpage.html', context)
 
-def part1(request):
 
+def part1(request):
     if request.method == 'POST':
         form = AlcoholForm(request.POST)
         if form.is_valid():
-
             alcohols = request.POST.getlist('alcoholToChoice')
             request.session['alcohols'] = alcohols
             return HttpResponseRedirect('otherdrink')
@@ -25,17 +23,19 @@ def part1(request):
         form = AlcoholForm()
         return render(request, 'searchdrink/part1-choosealcohol.html', {'form': form})
 
+
 def part2(request):
     if request.method == 'POST':
         form = OtherDrinkForm(request.POST)
         if form.is_valid():
             otherdrinks = request.POST.getlist('otherdrinkToChoice')
-            request.session['otherdrinks']=otherdrinks
+            request.session['otherdrinks'] = otherdrinks
             return HttpResponseRedirect('fruit')
     else:
         form = OtherDrinkForm()
 
     return render(request, 'searchdrink/part2-chooseotherdrinks.html', {'form': form})
+
 
 def part3(request):
     if request.method == 'POST':
@@ -49,6 +49,7 @@ def part3(request):
 
     return render(request, 'searchdrink/part3-choosefruits.html', {'form': form})
 
+
 def part4(request):
     if request.method == 'POST':
         form = OtherAddForm(request.POST)
@@ -61,35 +62,30 @@ def part4(request):
 
     return render(request, 'searchdrink/part4-chooseotheradd.html', {'form': form})
 
+
 def results(request):
-    alcohols = request.session['alcohols']
-    otherdrinks = request.session['otherdrinks']
-    fruits = request.session['fruits']
-    otheradds = request.session ['otheradds']
+    ingredients = request.session['alcohols'], request.session['otherdrinks'], request.session['fruits'], \
+                  request.session['otheradds']
 
-    selected_alcohols = Alcohol.objects.filter(id__in=alcohols)
-    selected_otherdrinks = OtherDrink.objects.filter(id__in=otherdrinks)
-    selected_fruits = Fruit.objects.filter(id__in=fruits)
-    selected_otheradds = OtherAdd.objects.filter(id__in=otheradds)
+    selected_alcohols = Alcohol.objects.filter(id__in=ingredients[0])
+    selected_otherdrinks = OtherDrink.objects.filter(id__in=ingredients[1])
+    selected_fruits = Fruit.objects.filter(id__in=ingredients[2])
+    selected_otheradds = OtherAdd.objects.filter(id__in=ingredients[3])
 
+    filter_by_alcohol = Drink.objects.filter(_alcohol__in=selected_alcohols)
+    filter_by_otherdrinks = Drink.objects.filter(_otherdrink__in=selected_otherdrinks)
+    filter_by_fruits = Drink.objects.filter(_fruit__in=selected_fruits)
+    filter_by_otheradds = Drink.objects.filter(_otheradd__in=selected_otheradds)
 
-    a = Drink.objects.filter(_alcohol__in=selected_alcohols)
-    b = Drink.objects.filter(_otherdrink__in=selected_otherdrinks)
-    c = Drink.objects.filter(_fruit__in=fruits)
-    d = Drink.objects.filter(_otheradd__in=selected_otheradds)
-
-    allDrinks = a | b | c | d
-    checking = get_list_or_404(allDrinks)
-    result_list = list(chain(a, b, c, d))
-    results_dict = {i:result_list.count(i) for i in result_list}
-
+    checking = get_list_or_404(filter_by_alcohol | filter_by_otherdrinks | filter_by_fruits | filter_by_otheradds)
+    result_list = list(chain(filter_by_alcohol, filter_by_otherdrinks, filter_by_fruits, filter_by_otheradds))
+    results_dict = {i: result_list.count(i) for i in result_list}
     for key, value in results_dict.items():
         generalAmount = key._alcohol.count() + key._otherdrink.count() + key._otheradd.count() + key._fruit.count()
-        results_dict[key] = value/generalAmount
+        results_dict[key] = value / generalAmount
 
     sorted_results = {k: v for k, v in sorted(results_dict.items(), key=lambda item: item[1], reverse=True)}
     sorted_results_iter = iter(sorted_results)
-    print(sorted_results)
     bestChoice = next(sorted_results_iter)
 
     if len(sorted_results) > 1:
@@ -106,6 +102,7 @@ def results(request):
 
     return render(request, 'searchdrink/results.html', {'bestChoice': bestChoice})
 
+
 def result2(request):
     if request.session.get('bestChoice_second_json', False):
         bestChoice_second_json = request.session['bestChoice_second_json']
@@ -113,16 +110,16 @@ def result2(request):
         for dicts in bestChoice_second_list:
             for key, values in dicts.items():
                 for k, v in dicts['fields'].items():
-                    bestChoice_second  = Drink.objects.get(nameDrink__contains=dicts['fields']['nameDrink'])
+                    bestChoice_second = Drink.objects.get(nameDrink__contains=dicts['fields']['nameDrink'])
     else:
         return render(request, 'searchdrink/404.html', {})
 
     return render(request, 'searchdrink/results2.html', {'bestChoice_second': bestChoice_second})
 
 
-
 def bad_request(request, exception):
     return render(request, 'searchdrink/404.html', {})
+
 
 def searchbyname(request):
     if request.method == 'POST':
@@ -136,7 +133,6 @@ def searchbyname(request):
     else:
         form = NameDrinkForm()
 
-
     return render(request, 'searchdrink/searchbyname.html', {'form': form})
 
 
@@ -147,10 +143,6 @@ def searchnameresult(request):
         for key, values in dicts.items():
             for k, v in dicts['fields'].items():
                 finalresult = Drink.objects.get(nameDrink=dicts['fields']['nameDrink'])
-
-
+                print(type(finalresult))
 
     return render(request, 'searchdrink/searchbynameResult.html', {'finalresult': finalresult})
-
-
-
